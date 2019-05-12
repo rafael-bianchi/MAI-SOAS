754586224fb88ae58a783086ba0ba6b40c69e886
Rafael Bianchi <rafaellfontana@gmail.com>
2019-05-12 19:30:27 +0200

    agent path routing

--- soas_project/model.py
+++ soas_project/model.py
@@ -1,6 +1,8 @@
 from mesa import Model, Agent
 from mesa.space import MultiGrid
-from mesa.time import RandomActivation
+from mesa.time import SimultaneousActivation
+from .utils import getShortestPaths
+
 import random
 
 class Road(Agent):
@@ -25,26 +27,16 @@ class Walker(Agent):
         self.move()
 
     def move(self):
-        possible_steps = self.model.grid.get_neighborhood(
-            self.pos,
-            moore=False,
-            include_center=False)
+        possible_steps = self.city_roads[self.pos]
         
         r = random.SystemRandom()
 
         r.shuffle(possible_steps)
         
-        moved = False
-        while(len(possible_steps) > 0 and not moved):
-            pos = possible_steps.pop(0)
+        pos = possible_steps[0]
 
-            this_cell = self.model.grid.get_cell_list_contents([pos])
-
-            road = [obj for obj in this_cell if isinstance(obj, Road)]
-
-            if(len(road) > 0):
-                self.model.grid.move_agent(self, pos)
-                moved = True
+        self.model.grid.move_agent(self, pos)
+        self.pos = pos
 
 class CityModel(Model):
     def __init__(self, N=2, width=20, height=10, city_map=[]):
@@ -52,7 +44,7 @@ class CityModel(Model):
         self.N = N    # num of agents
         self.headings = ((1, 0), (0, 1), (-1, 0), (0, -1))  # tuples are fast
         self.grid = MultiGrid(width, height, torus=False)
-        self.schedule = RandomActivation(self)
+        self.schedule = SimultaneousActivation(self)
         
         unique_id_counter = 0
         unique_id_counter = self.create_map(city_map, width, height, unique_id_counter)
@@ -61,20 +53,37 @@ class CityModel(Model):
         self.running = True
 
     def create_map(self, city_map, width, height, unique_id_counter):
+        self.city_roads = {}
         for idx_line, line in enumerate(city_map):
            for idx_col, cell in enumerate(line):
                 agent = None
-                pos = ((height - 1) - idx_line, idx_col)
+                pos = (idx_line, (height - 1) - idx_col)
                 if(cell == '1'):
-                   agent = Grass(unique_id_counter, self, pos)
+                    agent = Grass(unique_id_counter, self, pos)
                 elif(cell == '0'):
-                   agent = Road(unique_id_counter, self, pos)
+                    agent = Road(unique_id_counter, self, pos)
+                    neighbors = set()
+
+                    for next_pos in [(0,-1), (-1, 0), (1,0), (0,1)]:
+                        node_position = (pos[0] + next_pos[0], pos[1] + next_pos[1])
+                        if node_position[0] > width - 1 or node_position[0] < 0 or node_position[1] > height - 1 or node_position[1] < 0:
+                            continue
+
+                        # Make sure walkable terrain
+                        if city_map[node_position[0]][node_position[1]] != '0':
+                            continue
+
+                        neighbors.add(node_position)
+                    
+                    self.city_roads[pos] = neighbors
 
                 #print(f'Adding {type(agent)} on {pos}')
                 self.schedule.add(agent)
                 self.grid.place_agent(agent, pos)
 
                 unique_id_counter = unique_id_counter + 1
+
+        self.shortest_paths = getShortestPaths(self.city_roads)
 
         return unique_id_counter
     def make_walker_agents(self, unique_id_counter):
--- soas_project/server.py
+++ soas_project/server.py
@@ -28,7 +28,6 @@ def agent_draw(agent):
     elif isinstance(agent, Walker):
         portrayal = {
             "Shape": "arrowHead",
-            "Filled": "true",
             "Layer": 1,
             "Color": ["#0066ff", "#005ce6"],
             "stroke_color": "#666666",
@@ -44,25 +43,28 @@ def agent_draw(agent):
 
 
 def launch_city_model():
-    city_map = []
+    city_roads = []
 
-    with open("city_map.txt","r") as f:
+    with open("city_map_21x21.txt","r") as f:
         for line in f:
             l = []
             for cell in line.strip().split('\t'):
                 l.append(cell)
                 print(f'{cell}', end =" ")
-            city_map.append(l)
+            city_roads.append(l)
 
-    height = len(city_map)
-    width = len(city_map[0])
+    height = len(city_roads)
+    width = len(city_roads[0])
 
     num_agents = 6
-    pixel_ratio = 15
+    pixel_ratio = 40
+
+    #shortest_paths = utils.
+
     grid = CanvasGrid(agent_draw, width, height,
                       width * pixel_ratio, height * pixel_ratio)
     server = ModularServer(CityModel, [grid], "SOAS Project - Rafael Bianchi",
-                           {"N": num_agents, "width": width, "height": height, "city_map": city_map})
+                           {"N": num_agents, "width": width, "height": height, "city_map": city_roads})
     server.max_steps = 0
     server.port = 8521
     server.launch()
